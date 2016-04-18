802d209621b2769679d09deee3bcfea17ae4aad6
Uche Eke <uche.eke@gmail.com>
2016-04-18 18:49:54 +0100

    Initial commit

---
+++ dev/components/app.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by ekeu on 18/04/16.
+ */
+
+import {Component} from 'angular2/core';
+
+@Component({
+    selector : "my-app",
+    templateUrl: "tpls/app.html"
+}) export class AppComponent {
+
+}
