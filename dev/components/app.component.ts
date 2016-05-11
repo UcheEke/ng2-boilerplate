4ac38c0ab09f5427814507cf5c8728585d01bdbc
Uche Eke <uche.eke@gmail.com>
2016-05-11 18:48:05 +0100

    Updated for rc-1

--- dev/components/app.component.ts
+++ dev/components/app.component.ts
@@ -1,12 +1,6 @@
-/**
- * Created by ekeu on 18/04/16.
- */
-
-import {Component} from 'angular2/core';
-
+import { Component } from '@angular/core';
 @Component({
-    selector : "my-app",
-    templateUrl: "tpls/app.html"
-}) export class AppComponent {
-
-}
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
